1b27361aaa0df11c6eecbb497f7ba2c533026b6f
Bard Langoy <bard.langoy@gmail.com>
2011-04-07 23:12:46 +0200

    http://jira.codehaus.org/browse/MILYN-574

--- edi/ect/src/main/java/org/milyn/ect/EdiConvertionTool.java
+++ edi/ect/src/main/java/org/milyn/ect/EdiConvertionTool.java
@@ -65,9 +65,9 @@ public class EdiConvertionTool {
      * @param urn The URN for the EDI Mapping model configuration set.
      * @throws IOException Error writing Mapping Model configuration set.
      */
-    public static void fromUnEdifactSpec(ZipInputStream specification, ZipOutputStream modelSetOutStream, String urn) throws IOException {
+    public static void fromUnEdifactSpec(ZipInputStream specification, ZipOutputStream modelSetOutStream, String urn, boolean useShortName) throws IOException {
         try {
-            fromSpec(new UnEdifactSpecificationReader(specification, true), modelSetOutStream, urn);
+            fromSpec(new UnEdifactSpecificationReader(specification, true, useShortName), modelSetOutStream, urn);
         } finally {
             specification.close();
         }
@@ -105,9 +105,9 @@ public class EdiConvertionTool {
      * @param urn The URN for the EDI Mapping model configuration set.
      * @throws IOException Error writing Mapping Model configuration set.
      */
-    public static void fromUnEdifactSpec(ZipInputStream specification, File modelSetOutFolder, String urn) throws IOException {
+    public static void fromUnEdifactSpec(ZipInputStream specification, File modelSetOutFolder, String urn, boolean useShortName) throws IOException {
         try {
-            fromSpec(new UnEdifactSpecificationReader(specification, true), modelSetOutFolder, urn);
+            fromSpec(new UnEdifactSpecificationReader(specification, true, useShortName), modelSetOutFolder, urn);
         } finally {
             specification.close();
         }
